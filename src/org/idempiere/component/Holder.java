c9dc5b2119f93477a97d7806826858a028ed0acf
Ahmad Anwar Ibrahim <ahmad.anwar.ibrahim@gmail.com>
2022-09-16 22:01:46 +0800

    first commit

---
+++ src/org/idempiere/component/Holder.java
@@ -0,0 +1,19 @@
+package org.idempiere.component;
+
+import org.adempiere.base.event.AbstractEventHandler;
+import org.osgi.service.event.Event;
+
+public class Holder extends AbstractEventHandler  {
+	
+
+		@Override 
+		protected void initialize() { 
+			}
+
+
+		@Override
+		protected void doHandleEvent(Event event) {
+			// TODO Auto-generated method stub
+			
+		}
+}
